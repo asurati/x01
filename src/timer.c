84be3da6fcab274a63f5d4f4fe1a0e4ab68e5397
Amol Surati <asurati@users.noreply.github.com>
2018-01-16 09:56:43 +0530

    timer: timer irq must disable the timer.

--- src/timer.c
+++ src/timer.c
@@ -17,7 +17,32 @@
 
 #include <assert.h>
 #include <io.h>
+#include <irq.h>
 #include <timer.h>
+
+/* RPi1 and RPi2 have System Timer at PA 0x20003000 and 0x3f003000,
+ * respectively. A System Timer is implemented by the SoC.
+ *
+ * RPi2 additionally has Generic Timers, also called Core Timers,
+ * which are implemented within the CPU.
+ *
+ * QRPI2 does not implement the System Timer.
+ *
+ * Hence, depending on the board selected, either the System Timer
+ * or the Generic Timer must be implemented.
+ */
+
+static int timer_irq(void *data)
+{
+	uint32_t v;
+	data = data;
+	asm volatile("mrc	p15, 0, %0, c14, c2, 1"
+		     : "=r" (v));
+	v &= ~1;
+	asm volatile("mcr	p15, 0, %0, c14, c2, 1"
+		     : : "r" (v));
+	return 0;
+}
 
 #ifdef QRPI2
 void timer_init()
@@ -25,9 +50,12 @@ void timer_init()
 	uint32_t cntdn;
 	volatile uint32_t *ctl;
 
-	/* Connect the cntpnsirq timer to the FIQ. */
+	irq_insert(IRQ_DEV_TIMER, timer_irq, NULL);
+
+	/* QA7_rev3.4.
+	 * Connect the cntpnsirq to the CPU IRQ interrupt. */
 	ctl = (volatile uint32_t *)(ctrl_base + 0x40);
-	*ctl |= 1 << 5;
+	*ctl |= 1 << 1;
 
 	cntdn = 62500000;
 
@@ -41,8 +69,10 @@ void timer_init()
 	cntdn |= 1;
 	asm volatile("mcr	p15, 0, %0, c14, c2, 1"
 		     : : "r" (cntdn));
-
 }
 #else
-#define timer_init	do {} while (0)
+void timer_init()
+{
+	irq_insert(IRQ_DEV_TIMER, timer_irq, NULL);
+}
 #endif
